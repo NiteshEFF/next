3e6e17cacfcab604699ae675caa01ca3fd619b43
Niteshkumartgw <nitesh.kumar@thegrowthwings.com>
2021-01-13 20:41:14 +0530

    value-malue

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,7 +1,7 @@
 import {useRouter} from 'next/router';
-import {getMovieById} from '../actions';
+import {getMovieById} from '../../actions';
 
-const Movie = () =>{
+const Movie = (props) =>{
     const router = useRouter();
     const {id} = router.query;
 
@@ -9,7 +9,7 @@ const Movie = () =>{
         <div className="container">
             <h3>Movie with id: {id}</h3>
             <div className="jumbotron">
-        <h1 className="display-4">{ }</h1>
+        <h1 className="display-4">{props.selectedmovie.name}</h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr className="my-4" />
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -23,7 +23,9 @@ const Movie = () =>{
 };
 
 Movie.getInitialProps =() => {
-    return get
+    return getMovieById("2").then(movie => {
+        return {selectedmovie:movie}
+    })
 }
 
 export default Movie;
